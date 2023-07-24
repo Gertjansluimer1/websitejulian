7cbed28e5ba657f6b04c4f42853f606b239f7252
Gertjansluimer1 <133050845+Gertjansluimer1@users.noreply.github.com>
2023-07-24 23:29:33 +0200

    Add files via upload

--- JS/slider.js
+++ JS/slider.js
@@ -1,9 +1,11 @@
 const carousel = document.querySelector(".carousel");
 
+var aanuit = true;
 
 let isDraqgging = false, startX, startScrollLeft;
 
 const dragStart = (e) => {
+    aanuit = false;
     isDraqgging = true;
     carousel.classList.add("dragging");
     startX = e.pageX;
@@ -16,18 +18,27 @@ const dragging = (e) => {
 }
 
 const dragStop = () => {
+    aanuit = true;
     isDraqgging = false;
     carousel.classList.remove("dragging");
 }
 
-var count = 0;
-var timer = setInterval(counter, 3000)
+setInterval(autoSwipe, 2000)
 
-function counter() {
-    console.log(++count);
-    carousel.scrollLeft = carousel.scrollLeft +screen.width;
+function autoSwipe() {
+    console.log(aanuit);
+    if(aanuit) {
+        carousel.scrollLeft = carousel.scrollLeft + screen.width;
+    }
 }
+
+
+//carousel.scrollLeft = carousel.scrollLeft + screen.width;
+
 
 carousel.addEventListener("mousemove", dragging);
 carousel.addEventListener("mousedown", dragStart);
 document.addEventListener("mouseup", dragStop);
+
+carousel.addEventListener("touchstart", dragStart);
+document.addEventListener("touchend", dragStop);
